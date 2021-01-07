1f5c0300e3e6cce31c1db7578fc6c61110da3f54
Arjun Gandhi <arjungandhi06@gmail.com>
2021-01-07 15:38:13 -0500

    test

--- bots/msg_tobor.py
+++ bots/msg_tobor.py
@@ -33,7 +33,7 @@ def main():
             return
         elif message.content.startswith(perms['command_key']) and message.channel.id in perms['super']['channels']:
             message = pop_command_key(message, perms)
-            
+            print(print(repr(message)))
 
             args = message.content.split(' ')
             print(args)
