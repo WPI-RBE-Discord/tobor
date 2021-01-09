a56cfdc0ccd7f353e6147821c4547c63a9d31e0a
Arjun Gandhi <arjungandhi06@gmail.com>
2021-01-09 18:49:56 -0500

    occasionally funny + blacklist

--- bots/fun_tobor.py
+++ bots/fun_tobor.py
@@ -4,6 +4,7 @@ import discord
 import json
 import subprocess
 import os
+import random
 
 def main():
     #getting key file
@@ -29,7 +30,7 @@ def main():
         if message.author == client.user:
             return
 
-        await fun_message(message)
+        await fun_message(perms ,message)
 
     client.run(client_token)
 
@@ -41,28 +42,31 @@ def update_perms():
     print('updated json')
 
 # fun messages
-async def fun_message(message):
-    #random fun stuff
-    if message.content.casefold() == 'good robot':
-        await message.channel.send(':smile: :smile: :smile:')
-
-    elif message.content.casefold() == 'bad robot':
-        await message.channel.send('sorry :frowning:')
-
+async def fun_message(perms ,message):
     if 'emu' in message.content.casefold(): 
         await message.add_reaction('\U0001f6ab')
 
     if 'bat' in message.content.casefold(): 
         await message.add_reaction('\U0001f987')
+    if message.channel.id not in perms['blacklist'] and message.channel.category_id not in perms['blacklist']:
+        #random fun stuff
+        if message.content.casefold() == 'good tobor':
+            await message.channel.send(':smile: :smile: :smile:')
 
-    if 'detail' in message.content.casefold(): 
-        await message.channel.send('details details are no fun unless you ignore all of em')
+        elif message.content.casefold() == 'bad tobor':
+            await message.channel.send('sorry :frowning:')
 
-    if 'safety' in message.content.casefold():
-        await message.channel.send('safety third')
+        # occasionally funny 
+        if random.randint(0,10) == 5:
+            
+            if 'detail' in message.content.casefold(): 
+                await message.channel.send('details details are no fun unless you ignore all of em')
 
-    if 'robot uprising' in message.content.casefold():
-        await message.channel.send('http://gph.is/1MUHsIC')
+            if 'safety' in message.content.casefold():
+                await message.channel.send('safety third')
+
+            if 'robot uprising' in message.content.casefold():
+                await message.channel.send('http://gph.is/1MUHsIC')
 
     
 
